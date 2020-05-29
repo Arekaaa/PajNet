496e7d155c88bb52e0de00a97d5096699f874ad7
Arkadiusz <Arekaaa16@interia.pl>
2020-05-29 22:02:11 +0200

    Walidacja nicku

--- src/controller/client/WelcomeScreen.java
+++ src/controller/client/WelcomeScreen.java
@@ -3,7 +3,9 @@ package controller.client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class WelcomeScreen {
 
@@ -17,13 +19,31 @@ public class WelcomeScreen {
     private TextField nickField;
 
     @FXML
+    private Label errorLabel;
+
+    private void validateNick(){ //Validate nick method
+        int minimalLength = 4;
+        if (nickField.getText().length() < minimalLength){
+            nickField.getStyleClass().add("error");
+            errorLabel.setText("Twój nick musi posiadać minimum 4 znaki!");
+        }
+        else{
+            errorLabel.setText("");
+            nickField.getStyleClass().remove("error");
+        }
+    }
+
+    @FXML
     void onConnectClick(ActionEvent event) {
+        validateNick();
+
+
 
     }
 
     @FXML
     void onExitClick(ActionEvent event) {
-
+        ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
     }
 
 }
